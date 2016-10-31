03255a2708f94a16fb210ade89abec551f2d7974
alex <you@example.com>
2016-10-31 08:21:37 +0300

    alex

---
+++ database/migrations/2016_10_13_180726_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_category')->nullable();
+            $table->foreign('parent_category')
+                ->references('id')
+                ->on('categories')
+                ->onUpdate('cascade');
+            $table->string('category_name');
+            $table->string('code');
+            $table->boolean('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
