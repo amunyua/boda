aac2734e3d1c2da53baaec5e8c4ec590e312e014
Eric Sensus <eric@sensussystems.com>
2017-05-23 16:49:24 +0300

    Clean up fap

--- app/Http/Controllers/FirstApplicationsController.php
+++ app/Http/Controllers/FirstApplicationsController.php
@@ -76,9 +76,9 @@ class FirstApplicationsController extends Controller
                     // send sms with login credentials
                     $bc = new BroadcastController();
 
-                    $message = 'Dear '.$request->firstname.'.';
-                    $message .= 'Welcome to the Boda Squared family.';
-                    $message .= 'Your login credentials are as follows: Email: '.$request->email.', Password: '.$plain_pass;
+                    $message = 'Dear '.$request->firstname.'. ';
+                    $message .= 'Welcome to the Boda Squared family. ';
+                    $message .= 'Your login credentials are as follows: Email: '.$request->email.', Password: '.$plain_pass.' ';
                     $message .= 'After successful verification. ';
                     $message .= 'Go to http://bodasquared.co.ke/boda/public and login to complete your application!';
                     $bc->sendSms($phone_no, $message);
