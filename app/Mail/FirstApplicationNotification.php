9141fba293def29e921dec19c3b0b8cecba8e9ae
Eric Murimi <emurinyo@gmail.com>
2017-05-24 18:41:27 +0300

    Add Confirmation token to users Create notification for admin on fap add a folder to store the official company logo

---
+++ app/Mail/FirstApplicationNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class FirstApplicationNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $candidate;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param $candidate
+     */
+    public function __construct($candidate)
+    {
+        $this->candidate = $candidate;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mails.fapnotice');
+    }
+}
