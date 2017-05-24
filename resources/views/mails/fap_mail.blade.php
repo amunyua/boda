0de671137fd2cbc2979a91cda1542157e41ff997
Eric Murimi <emurinyo@gmail.com>
2017-05-24 19:13:42 +0300

    Correct fap message

--- resources/views/mails/fap_mail.blade.php
+++ resources/views/mails/fap_mail.blade.php
@@ -1,7 +1,6 @@
-<p>Dear <b>$user->name</b>,</p>
-<p>
-    Congratulations, your <b>First Application</b> has been successfully approved.
-    Please login to proceed to the <b>Second Application (this is actually the second last step in the application process).</b>.
-</p>
+<p>Dear <b>{{ $user->name }}</b>,</p>
+<p>Congratulations, your <b>First Application</b> has been successfully approved.</p>
+<p>Please login to proceed to the <b>Second Application</b>
+    (<i>this is actually the second last step in the application process</i>).</p>
 <p>The <b>Third and final step,</b> will involve signing of a contract and your being given a bike!</p>
 <p>All the best!</p>
